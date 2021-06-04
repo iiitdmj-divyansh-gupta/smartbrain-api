93f9a67472795fb0ebfd972dff77e2ce3edae78b
Divyansh Gupta <dggupta0812@gmail.com>
2021-06-04 17:53:43 +0530

    refactoring

--- server.js
+++ server.js
@@ -28,10 +28,7 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-	// res.send(database.users);
-	res.send('It is working');
-})
+app.get('/', (req, res) => {res.send('It is working')});
 
 // dependency injection
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
